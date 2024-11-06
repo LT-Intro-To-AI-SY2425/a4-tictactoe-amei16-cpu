d32c95b08b776f5a28a25b090f2c88ac5fdfc07f
Aden Mei <amei16@cps.edu>
2024-11-06 11:36:43 -0600

    finished a4 and finished write up

--- notes.py
+++ notes.py
@@ -2,3 +2,23 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    def __init__(self, n = "no name", a = 0):
+        self.name = n
+        self.age = a
+
+    def __str__(self) -> str:
+        if self.age == 1:
+            s = f"{self.name} is {self.age} year old"
+        else:
+            s = f"{self.name} is {self.age} years old"
+        return s
+
+
+logan = Dog("logan", 8)
+cookie = Dog()
+maple = Dog("maple", 1)
+
+print(logan.name)
+print(cookie)
+print(maple)
